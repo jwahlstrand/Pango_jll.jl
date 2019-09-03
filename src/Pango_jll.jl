833dfecfeec9fa877a2ba65f1b7e7ca784a85fcb
Elliot Saba <staticfloat@gmail.com>
2019-09-03 01:02:14 -0700

    Pango_jll build 1.42.4+0

---
+++ src/Pango_jll.jl
@@ -0,0 +1,26 @@
+module Pango_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("36c8627f-9965-5494-a995-c6b170f724f3"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Pango", artifacts_toml) for e in artifacts["Pango"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Pango; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Pango_jll
